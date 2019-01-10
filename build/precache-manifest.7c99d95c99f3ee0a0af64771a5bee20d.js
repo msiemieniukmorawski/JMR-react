667d8f277a0b22c6060b532af45a0098ea917962
MS-M <msiemieniukmorawski@gmail.com>
2019-01-10 18:03:50 +0100

    after biuld

---
+++ build/precache-manifest.7c99d95c99f3ee0a0af64771a5bee20d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39a11f15e2dd82933a569acb3ef28864",
+    "url": "/static/media/background.39a11f15.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b230a10fb309e402309b",
+    "url": "/static/js/main.b230a10f.chunk.js"
+  },
+  {
+    "revision": "97a498d2cddd4e6e731d",
+    "url": "/static/js/1.97a498d2.chunk.js"
+  },
+  {
+    "revision": "b230a10fb309e402309b",
+    "url": "/static/css/main.63c3080a.chunk.css"
+  },
+  {
+    "revision": "76790623a3d8a1bd9a233a6d6595053d",
+    "url": "/index.html"
+  }
+];
